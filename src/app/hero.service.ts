6451e13a1ce247b406c30bab9b54036705053bd0
Evan Cleary <evanjcleary@gmail.com>
2020-03-30 22:12:40 -0400

    completed tour of heroes app

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -17,12 +16,12 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes'; //URL to web api  
   private httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`)
   }
-  private handleError<T> (operation = 'operation', result?: T) {
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       console.error(error);
 
@@ -58,7 +57,40 @@ export class HeroService {
     )
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id =${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    )
+  }
 
-// https://angular.io/tutorial/toh-pt6#add-a-new-hero
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    )
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+        this.log(`found heroes matching "${term}"`) :
+        this.log(`no heroes matching "${term}"`)),
+
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
+
+
+  // https://angular.io/tutorial/toh-pt6#add-a-new-hero
 
 }
